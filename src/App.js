4ccca225f0e06694e5ae3c7b61c5025ef8f526ff
Kevin Ge <kevin.ge@mail.utoronto.ca>
2019-11-05 01:38:26 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+ 
+    return (
+      <div>      
+  <div class="card">
+          <img class="card-img-top" src={this.props.pokePic} alt="Card image cap"></img>
+          <div class="card-body">
+            <h5 class="card-title">{this.props.name}</h5>
+
+          </div>
+        </div>
+   
+        
     </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      pokemonNumber: 1,
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon-form/"+ this.state.pokemonNumber)
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result.sprites.front_default);
+        this.setState({
+          isLoaded: true,
+          items: result,
+          pokePic: result.sprites.front_default,
+        })
+
+      });
+
+
+  }
+
+  handleClick(i){
+    
+
+  }
+
+  render() {
+    // console.log(this.state.items.version_group);
+    return (
+      <>
+      <Card pokePic={this.state.pokePic} name={this.state.items.name}/>
+      
+      </>
+    );
+  }
 }
 
 export default App;
